b9de43275449458960d2b2d3d3601a78e2ba50aa
ROMAIN LEMONNIER <lemonnier.romain13@gmail.com>
2024-11-12 11:55:18 +0100

    add all changes v2 test

--- client/src/components/CardsTheme.tsx
+++ client/src/components/CardsTheme.tsx
@@ -2,25 +2,25 @@ import CardsEvent from "./CardsEvent";
 import "../styles/CardsTheme.css";
 
 interface CardsThemeI {
-	name: string;
+  name: string;
 }
 
 export default function CardsTheme({ name }: CardsThemeI) {
-	return (
-		<>
-			<section className="cards-theme">
-				<div className="title-card-event">
-					<h2 className="title-theme">{name}</h2>
-					<p className="voir-plus">voir plus</p>
-				</div>
-				<div className="event-align">
-					<CardsEvent />
-					<CardsEvent />
-					<div className="third-card">
-						<CardsEvent />
-					</div>
-				</div>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section className="cards-theme">
+        <div className="title-card-event">
+          <h2 className="title-theme">{name}</h2>
+          <p className="voir-plus">voir plus</p>
+        </div>
+        <div className="event-align">
+          <CardsEvent />
+          <CardsEvent />
+          <div className="third-card">
+            <CardsEvent />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
