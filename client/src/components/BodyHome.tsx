4780ed399f6c0eaf2fcb47498832ebd55f570e81
ROMAIN LEMONNIER <lemonnier.romain13@gmail.com>
2024-11-12 17:02:27 +0100

    add themes-components on the home page - small changement

--- client/src/components/BodyHome.tsx
+++ client/src/components/BodyHome.tsx
@@ -5,10 +5,10 @@ export default function BodyHome() {
   return (
     <>
       <section className="section-body-home">
-        <CardsTheme name="Evènements artistiques" />
+        <CardsTheme name="Evènements musicaux" />
         <CardsTheme name="Evènements sportifs" />
         <CardsTheme name="Spectacles" />
-        <CardsTheme name="Evènements musicaux" />
+        <CardsTheme name="Evènements artistiques" />
       </section>
     </>
   );
