fc7334726e97e1f18184df2cccb4aeaae6eb69c3
vloiseau13 <valentin.loiseau13@gmail.com>
2024-11-15 15:47:39 +0100

    Event card on the front page

--- client/src/components/CardsEvent.tsx
+++ client/src/components/CardsEvent.tsx
@@ -1,20 +1,37 @@
+import { useEffect, useState } from "react";
 import "../styles/CardsEvent.css";
 
+interface Event {
+  nom: string;
+  media_url: string;
+  date: string;
+  description_evt: string;
+  lieu: string;
+  url_site: string;
+}
+
 export default function CardsEvent() {
+  const [event, setEvent] = useState<Event | null>(null);
+  useEffect(() => {
+    fetch(
+      "https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/244400404_agenda-evenements-nantes-metropole_v2@nantesmetropole/records?limit=19",
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setEvent(data.results[1]);
+      });
+  }, []);
+
   return (
     <>
       <section className="card-event-section">
-        <img src="/boobatest.jpg" alt="booba" />
+        <img src={event?.media_url} alt="booba" />
         <div className="event-details">
-          <h3>booba</h3>
-          <p>Zenith de Nantes - 06.12.24</p>
-          <p className="hide-text">
-            du 18 au 25 novembre 2024, spectacles, jeux, ateliers... sont
-            proposés gratuitement aux parents et aux enfants à la Maison des
-            Confluences, dans le cadre de la semaine de parentalité. Pendant les
-            ateliers, les enfants sont sous la surveillance des parents
-            Consultez le programme complet
+          <h3>{event?.nom}</h3>
+          <p>
+            {event?.lieu} {event?.date}
           </p>
+          <p className="hide-text">{event?.description_evt}</p>
         </div>
       </section>
     </>
