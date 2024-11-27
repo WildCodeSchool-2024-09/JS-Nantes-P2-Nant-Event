3250999aacb0908e4ee71821cbcfa8eddd5bae36
ben5978lab <ben.rose5978@gmail.com>
2024-11-27 17:12:05 +0100

    liaison de l'événement à la map

--- client/src/components/MapCard.tsx
+++ client/src/components/MapCard.tsx
@@ -4,11 +4,11 @@ import type { EventI } from "../types/Events";
 
 interface MapCardProps {
   data: EventI;
+  onSelect: (event: EventI) => void;
 }
 
-function MapCard({ data }: MapCardProps) {
+function MapCard({ data, onSelect }: MapCardProps) {
   const event = data;
-
   const dateString = event?.date || "";
   const [_, month, day] = dateString.split("-");
 
@@ -31,33 +31,39 @@ function MapCard({ data }: MapCardProps) {
   };
 
   return (
-    <article className="event-card">
-      <time className="date" dateTime={event?.date}>
-        <span className="date-month">{getMonthName(month)}</span>
-        <span className="date-day">{Number.parseInt(day) || "6"}</span>
-      </time>
-      <section className="event-info">
-        <h2 className="event-title">{event?.nom}</h2>
-        <div className="event-time">{event?.heure_debut}</div>
-        <address className="adress">
-          <h3 className="location-name">{data?.lieu}</h3>
-          <p className="location-address">{event?.adresse}</p>
-        </address>
-      </section>
-      <figure className="event-image-container">
-        <Link to={`/event/${event.id_manif}`}>
-          {event.media_url ? (
-            <img
-              className="event-img"
-              src={event.media_url}
-              alt="Affiche de l'évènement"
-            />
-          ) : (
-            <img className="event-img" src="/koala.jpg" alt="Koala" />
-          )}
-        </Link>
-      </figure>
-    </article>
+    <button
+      type="button"
+      onClick={() => onSelect(event)}
+      className="btn-container"
+    >
+      <article className="event-card">
+        <time className="date" dateTime={event?.date}>
+          <span className="date-month">{getMonthName(month)}</span>
+          <span className="date-day">{Number.parseInt(day) || "6"}</span>
+        </time>
+        <section className="event-info">
+          <h2 className="event-title">{event?.nom}</h2>
+          <div className="event-time">{event?.heure_debut}</div>
+          <address className="adress">
+            <h3 className="location-name">{data?.lieu}</h3>
+            <p className="location-address">{event?.adresse}</p>
+          </address>
+        </section>
+        <figure className="event-image-container">
+          <Link to={`/event/${event.id_manif}`}>
+            {event.media_url ? (
+              <img
+                className="event-img"
+                src={event.media_url}
+                alt="Affiche de l'évènement"
+              />
+            ) : (
+              <img className="event-img" src="/koala.jpg" alt="Koala" />
+            )}
+          </Link>
+        </figure>
+      </article>
+    </button>
   );
 }
 
--- client/src/pages/MapPage.tsx
+++ client/src/pages/MapPage.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import ContainerMapCard from "../components/ContainerMapCard";
 import NantesEventsMap from "../components/NantesEventsMap";
 import ScrollToTopButton from "../components/Scrolltotop";
+import type { EventI } from "../types/Events";
 
 function MapPage() {
+  const [selectedEvent, setSelectedEvent] = useState<EventI | null>(null);
+
   return (
     <>
-      <NantesEventsMap />
-      <ContainerMapCard />
+      <NantesEventsMap selectedEvent={selectedEvent} />
+      <ContainerMapCard onEventSelect={setSelectedEvent} />
       <ScrollToTopButton />
     </>
   );
